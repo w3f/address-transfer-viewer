3ba7065d3a3e726d00c1b686a28fa0c2509b44f5
laboon <laboon@gmail.com>
2023-01-23 16:42:42 +0100

    working for multi-order

--- viewer.py
+++ viewer.py
@@ -6,10 +6,77 @@ import requests
 
 import numpy as N
 
+
+ADDRESS = ""
+ORDER = ""
+API_KEY = ""
+NUM_ITEMS_PER_PAGE = 25
+MAX_PAGES = 10
+
+
 def print_list(l):
     for item in l:
         print(item)
 
+def print_transfers(in_list, out_list, order):
+    print("INCOMING TRANSFERS (ORDER " + str(order) + ")")
+    print_list(N.unique(in_list))
+    print("OUTGOING TRANSFERS (ORDER " + str(order) + ")")
+    print_list(N.unique(out_list))
+
+        
+# Returns a tuple - first element is incoming transfers, 2nd is outgoing
+def get_transactions(addr):
+
+    # Subscan API endpoint
+    URL = 'https://polkadot.api.subscan.io/api/scan/transfers'
+
+    # Headers needed for receiving content
+    HEADERS = {
+        'Content-Type': 'application/json',
+        'X-API-Key': API_KEY,
+    }
+    
+    in_list = []
+    out_list = []
+    
+    for j in range(0, MAX_PAGES):
+
+        json_data = {
+            'row': NUM_ITEMS_PER_PAGE,
+            'page': j,
+            'address': addr
+        }
+        r = requests.post(URL, headers=HEADERS, json=json_data)
+        rj = json.loads(r.text)
+
+        if rj is None:
+            break
+
+        if rj['data']['transfers'] is None:
+            break
+    
+        num_on_page = len(rj['data']['transfers'])
+
+        if  num_on_page == 0:
+            break
+
+        # print("Num on page = " + str(num_on_page))
+    
+        for j in range(0, num_on_page):
+
+            # print("Starting " + str(j) + "...")
+            # For each item on the page, get the Address
+
+            from_addr = rj['data']['transfers'][j]['from']
+            to_addr = rj['data']['transfers'][j]['to']
+        
+            if (to_addr == ADDRESS):
+                in_list.append(from_addr)
+            else:
+                out_list.append(to_addr)
+    return N.unique(in_list), N.unique(out_list)
+        
 
 # EXECUTION STARTS HERE
 
@@ -20,12 +87,6 @@ def print_list(l):
 # 2: Order - number of iterations (e.g. 1 = addresses that this address has transferred to, 2 = addresses that THOSE addresses have transferred to, etc.)
 # 3: API Key - Your subscan API key. See https://support.subscan.io/#introduction
 
-ADDRESS = ""
-ORDER = ""
-API_KEY = ""
-NUM_ITEMS_PER_PAGE = 25
-MAX_PAGES = 100
-INDICATOR_LENGTH = 0
 
 # Read in args from command line
 
@@ -41,64 +102,29 @@ else:
     ORDER = int(sys.argv[2])
     API_KEY = sys.argv[3]
 
-# Subscan API endpoint
-URL = 'https://polkadot.api.subscan.io/api/scan/transfers'
 
-# Headers needed for receiving content
-HEADERS = {
-    'Content-Type': 'application/json',
-    'X-API-Key': API_KEY,
-}
+# Get initial list (input and output)
+in_list, out_list = get_transactions(ADDRESS)
 
+print_transfers(in_list, out_list, 1)
 
-for j in range(0, 1):
-    # for j in range(0, MAX_PAGES):
-    # Get data from one page of Subscan results, searching for System.remark_with_event
-    # extrinsics. The request is then converted to JSON (rj variable).
+# If order > 0, start following only inputs on input path,
+# only outputs on output path
 
-    # Right now an invalid request from Subscan will make this die, would be nice to
-    # add something which fixes that.
+for i in range(1, ORDER):
 
-    json_data = {
-        'row': NUM_ITEMS_PER_PAGE,
-        'page': j,
-        'address': ADDRESS
-    }
-    r = requests.post(URL, headers=HEADERS, json=json_data)
-    rj = json.loads(r.text)
-
-    in_list = []
-    out_list = []
-
-    num_on_page = len(rj['data']['transfers'])
-
-    # print("Num on page = " + str(num_on_page))
+    new_in_list = []
+    new_out_list = []
     
-    for j in range(0, num_on_page):
-
-        # print("Starting " + str(j) + "...")
-        # For each item on the page, get the Address
-
-        from_addr = rj['data']['transfers'][j]['from']
-        to_addr = rj['data']['transfers'][j]['to']
+    for addr in in_list:
+        new_in_list.extend(get_transactions(addr)[0])
         
-        if (to_addr == ADDRESS):
-            in_list.append(from_addr)
-        else:
-            out_list.append(to_addr)
-        line = rj['data']['transfers'][j]['to']
-
-        # raw_vote = (rj2[0]['value'])
-        # addr = rj['data']['extrinsics'][j]['account_display']['address']
-        # extr = rj['data']['extrinsics'][j]['extrinsic_index']
-
-    print("INCOMING TRANSFERS")
-    print_list(N.unique(in_list))
-    print("OUTGOING TRANSFERS")
-    print_list(N.unique(out_list))
-
-
-        # This sleeps for 400 ms, to ensure that we don't overwhelm our free tier of
-        # API services from Subscan
-    time.sleep(0.4)
+    for addr in out_list:
+        new_out_list.extend(get_transactions(addr)[1])
+        
+    print_transfers(new_in_list, new_out_list, (i + 1))
+    
+    in_list = N.unique(new_in_list)
+    out_list = N.unique(new_out_list)
+    
 
